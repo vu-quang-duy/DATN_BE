d10d3099f3b3784a90b65c53fca8f660d2ffdb38
Tran Anh Duc <lebroncurry180@gmail.com>
2025-05-20 02:33:02 +0700

    Practice Test

---
+++ src/entities/exam/exam-vocabulary.entity.ts
@@ -0,0 +1,31 @@
+import { EntityNameConst } from 'src/constant/entity-name';
+import { DBColumn } from 'src/decorator/swagger.decorator';
+import { Entity, PrimaryGeneratedColumn, JoinColumn, ManyToOne } from 'typeorm';
+import { AbstractCreatedIdEntity } from '../entity.interface';
+// import { ExamB } from 'src/entitiesB/exam.entity';
+import { IsSwaggerNumber } from '../../decorator/swagger.decorator';
+import { Vocabulary } from '../vocabulary/vocabulary.entity';
+
+@Entity(EntityNameConst.EXAM_VOCABULARY)
+export class ExamVocabulary extends AbstractCreatedIdEntity {
+  @IsSwaggerNumber({ default: 1 })
+  @PrimaryGeneratedColumn({ type: 'bigint', name: 'vocabulary_exam_id' }) // Định nghĩa ID mới
+  vocabularyExamId: number;
+
+  @DBColumn({ type: 'bigint', name: 'vocabulary_id' })
+  vocabularyId: number;
+
+  @DBColumn({ type: 'bigint', name: 'exam_id' })
+  examId: number;
+
+  @DBColumn({ type: 'varchar', name: 'content' })
+  content: string;
+
+  // @ManyToOne(() => ExamB, (exam) => exam.vocabularies)
+  // @JoinColumn({ name: 'exam_id' })
+  // exam: ExamB;
+
+  @ManyToOne(() => Vocabulary, (vocabulary) => vocabulary.exams)
+  @JoinColumn({ name: 'vocabulary_id' })
+  vocabulary: Vocabulary;
+}
